b4cf185eb6f3fc5ac599cfcddd449f7bc71a2c07
lyudmila50 <81586606+lyudmila50@users.noreply.github.com>
2021-12-09 23:06:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const getPaises = async()=>{
+  const response= await fetch('https://api.covid19api.com/summary');
+  const data= response.json();
+  const paises= data;
+  const miPaisito = paises.Countries.find(Countries => Countries.Country === "Uruguay");
+  const casosNuevos= miPaisito.NewConfirmed;
+  const totalConfirmado= miPaisito.TotalConfirmed;
+  const nuevasMuertes= miPaisito.NewDeaths;
+  const totalMuertes= miPaisito.TotalDeaths;
+
+  document.getElementsByClassName("miDiv").innerHTML('<p>${casosNuevos}</p>');
+  document.getElementsByClassName("miDiv").innerHTML('<p>${totalConfirmado}</p>');
+  document.getElementsByClassName("miDiv").innerHTML('<p>${nuevasMuertes}</p>');
+  document.getElementsByClassName("miDiv").innerHTML('<p>${totalMuertes}</p>');
+
+  await getPaises()
+
+}
+
+  
+
+
+
+
